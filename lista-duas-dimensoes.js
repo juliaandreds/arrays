5ea2333d4120b5e362cdfd875585bfcaa70a923f
juliaandreds <julia.andre@hotmail.com>
2023-01-08 19:57:53 -0300

    lista 2 dimensões

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,8 @@
+var alunos = ['Ana', 'Julia', 'Marcelo', 'Beto', 'Francisca'];
+var notas = [10, 6, 5.5, 9, 3.1];
+
+listaAlunosENotas = [alunos,notas];
+
+console.log (`A/O aluna(o) da posição 1 alunos é:  ${listaAlunosENotas [0][2]} 
+A nota dessa/e aluno é:  ${listaAlunosENotas [1][2]}`);
+
