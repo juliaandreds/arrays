5b53c3928851c3ee7a96e7b8649ec1f98e558141
juliaandreds <julia.andre@hotmail.com>
2023-01-08 19:30:34 -0300

    usando splice

---
+++ usando-splice.js
@@ -0,0 +1,5 @@
+var alunos = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+//splice --> (param1 -> index do dado, param -> quantos quer retirar, param3 -> o que vc quer susbstituir)
+alunos.splice (1, 2, "Mauricio");
+console.log (alunos);
