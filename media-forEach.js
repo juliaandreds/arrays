0f82fdd8adf5cfa79b9bd2fa5e1f47d4a1fc8818
juliaandreds <julia.andre@hotmail.com>
2023-01-09 00:21:01 -0300

    media-forEach

--- media-forEach.js
+++ media-forEach.js
@@ -2,6 +2,8 @@ const notas = [10, 6.5, 8, 7.5];
 
 somaDasNotas = 0;
 
+//a vantagem é que usando o forEach, pode passar mais de um parâmetro, como nesse caso, que utilizamos a nota e seu índice.
+
 notas.forEach (function (nota, indice) {
     somaDasNotas += nota;
     console.log (indice);
