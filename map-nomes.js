65991e0845e0e62fd68e590c55957f240020e07d
juliaandreds <julia.andre@hotmail.com>
2023-01-09 00:58:28 -0300

    map nomes

---
+++ map-nomes.js
@@ -0,0 +1,8 @@
+const nomes = ["ana Julia","Caio vinicius", "BIA silva"];
+
+const nomeAtualizado = nomes.map ((nome) => {
+    return nome.toUpperCase ();
+    //toUpperCaso --> deixar em letras maiúsculas
+});
+
+console.log (nomeAtualizado)
