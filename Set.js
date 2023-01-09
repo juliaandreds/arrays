00e4d156bfbb237dfca0f6abe5d7b2108f9af6a5
juliaandreds <julia.andre@hotmail.com>
2023-01-09 15:25:06 -0300

    Removendo elementos repetidos

---
+++ Set.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const newSet = new Set (nomes);
+
+const nomesAtualizados = [...newSet(nomes)]
+
+console.log (nomesAtualizados);
