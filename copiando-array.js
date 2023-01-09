a77d5e3b52aaf6d2fa735735cf83dfb8afc019f3
juliaandreds <julia.andre@hotmail.com>
2023-01-09 15:14:42 -0300

    copiando array

---
+++ copiando-array.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+
+//[...] --> espalhamento, ou seja, ele mantém o original como ele é e em notas notas acrescenta o que foi necessário.
+const novasNotas = [...notas, 10];
+
+
+console.log (`As novas notas são: ${novasNotas}`);
+console.log (`As notas originais são: ${notas}`);
